536d4ad4d60437d0479119f74e25d16d33e1266a
Karol Jochelson <karol.jochelson@gmail.com>
2018-05-22 21:12:54 +0200

    Switched to coin market cap api

--- current_supply_clo.py
+++ current_supply_clo.py
@@ -32,47 +32,24 @@ async def on_ready():
 async def clo_current_supply(ctx):
     supply = get_current_supply()
 
-    btc_response = requests.get('https://api.coinmarketcap.com/v1/ticker/bitcoin/?convert=USD').json()[0]
-    btc_price = float(btc_response['price_usd'])
-    # eth_response = requests.get('https://api.coinmarketcap.com/v1/ticker/ethereum/?convert=USD').json()[0]
-    # eth_price = float(eth_response['price_usd'])
+    try:
+        item = requests.get('https://api.coinmarketcap.com/v2/ticker/2757/?convert=BTC').json()['data']
+    except KeyError:
+        item = None
 
-    ste_response = requests.get('https://stocks.exchange/api2/ticker').json()
-    ste_data = {i['market_name']: i for i in ste_response if i['market_name'] == u'CLO_BTC'}
+    if item:
+        price_usd = float(item['quotes']['USD']['price'])
+        price_btc = float(item['quotes']['BTC']['price'])
+        vol_btc = float(item['quotes']['BTC']['volume_24h'])
+        rank = item['rank']
 
-    item = ste_data.get(u'CLO_BTC', None)
-    # eth_item = ste_data.get(u'CLO_ETH', None)
-
-    if item and btc_price:
-        price_usd = float(item['last']) * btc_price
-        # eth_price_usd = float(eth_item['last']) * eth_price
-        sat = float(item['last'])
-        # eth = float(eth_item['last'])
-
-        vol = float(item['vol'])
-        # eth_vol = float(eth_item['vol'])
-
-        min24h = float(item['bid'])
-        max24h = float(item['ask'])
-
-        # eth_min24h = float(eth_item['bid'])
-        # eth_max24h = float(eth_item['ask'])
-
-        await client.say(ctx.message.author.mention + ': Here is your info for CLO')
         embed = discord.Embed()
-        embed.add_field(name = "Current Supply", value = "{}".format(localize(supply)))
+        embed.add_field(name = "Current Supply", value = "{}".format(localize(supply, decimals = 0)))
         embed.add_field(name = "Market Cap", value = "$ {}".format(localize(price_usd * supply)))
-        embed.add_field(name = "Volume CLO 24h", value = "{}".format(localize(vol, decimals = 4)))
-        embed.add_field(name = "Current BTC", value = "{}".format(localize(sat, decimals = 8)))
-        embed.add_field(name = "Min 24h BTC", value = "{}".format(localize(min24h, decimals = 8)))
-        embed.add_field(name = "Max 24h BTC", value = "{}".format(localize(max24h, decimals = 8)))
-        # embed.add_field(name = "Current ETH", value = "{}".format(localize(eth, decimals = 8)))
-        # embed.add_field(name = "Min 24h ETH", value = "{}".format(localize(eth_min24h, decimals = 8)))
-        # embed.add_field(name = "Max 24h ETH", value = "{}".format(localize(eth_max24h, decimals = 8)))
-        embed.add_field(name = "Price USD/BTC", value = "$ {}".format(localize(price_usd, decimals = 4)))
-        # embed.add_field(name = "Price USD/ETH", value = "$ {}".format(localize(eth_price_usd, decimals = 4)))
-        #embed.add_field(name = "Price Average", value = "$ {}".format(
-        #    localize((eth_price_usd + price_usd)/ 2, decimals = 4)))
+        embed.add_field(name = "Volume BTC 24h", value = "{}".format(localize(vol_btc, decimals = 4)))
+        embed.add_field(name = "Rank", value = "{}".format(rank))
+        embed.add_field(name = "Current BTC", value = "{}".format(localize(price_btc, decimals = 8)))
+        embed.add_field(name = "Current USD", value = "$ {}".format(localize(price_usd, decimals = 4)))
         await client.say(embed = embed)
     else:
         await client.say(ctx.message.author.mention + ': Sorry api did not return results')
